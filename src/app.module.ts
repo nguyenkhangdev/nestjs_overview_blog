1489bd89407d5fb01a4aa4ae4daff57215b92e8d
khang.trannguyen <nguyenkhangdev@gmail.com>
2025-09-24 17:12:35 +0700

    feat: post crud api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostModule } from './post/post.module';
+import { Post } from './post/entities/post.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs_blog',
+      entities: [Post],
+      synchronize: true,
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/post/post.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
+  create(createPostDto: CreatePostDto) {
+    const post = this.postRepository.create(createPostDto);
+    return this.postRepository.save(post);
+  }
+
+  findAll() {
+    return this.postRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.postRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    await this.postRepository.update(id, updatePostDto);
+    return this.findOne(id);
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} post`;
+  }
+}
